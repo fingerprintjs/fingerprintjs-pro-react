2682ab5bc1d73cc02c969ad84cc5ac7cf9940207
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-14 16:22:39 +0200

    style: fix linter

--- examples/preact/src/index.tsx
+++ examples/preact/src/index.tsx
@@ -4,11 +4,9 @@ import App from './components/app'
 import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react'
 
 const WrappedApp: FunctionalComponent = () => {
-  const apiKey = process.env.PREACT_APP_FPJS_PUBLIC_API_KEY as string;
+  const apiKey = process.env.PREACT_APP_FPJS_PUBLIC_API_KEY as string
   return (
-    <FpjsProvider
-      loadOptions={{ apiKey }}
-    >
+    <FpjsProvider loadOptions={{ apiKey }}>
       <App />
     </FpjsProvider>
   )
