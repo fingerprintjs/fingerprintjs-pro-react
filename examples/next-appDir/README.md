9c6236a1fda502c5555d3aae1346fcdc33084fff
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-02-03 19:13:59 +0100

    chore: add nextjs example with experimental

---
+++ examples/next-appDir/README.md
@@ -0,0 +1,24 @@
+## Setting up
+To try out these examples, you need to set your public API key as the value of the NEXT_PUBLIC_FPJS_PUBLIC_API_KEY variable\
+in the `.env.local`. You can just copy `.env` file located in this directory.
+
+More info about the API key you can find on the [documentation page](https://dev.fingerprint.com/docs/js-agent#agent-initialization).
+
+### Installing dependencies
+1. Go to the root of the project, install dependencies with `yarn install` and build it with `yarn build`.
+2. Come back to the example folder (`cd examples/next`) and run `yarn install`.
+
+After you've completed all the steps and inserted the API key, you can run:
+```shell
+yarn dev
+```
+
+Runs the app in the development mode.\
+Open [http://localhost:3002](http://localhost:3002) to view it in the browser.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
---
+++ examples/next-appDir/app/HomePage.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import styles from '../styles/Home.module.css'
+// @ts-ignore use direct path because of nextjs bug
+import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react/dist/fp-pro-react.cjs'
+import { useState } from 'react'
+
+const HomePage = () => {
+  const [extendedResult, updateExtendedResult] = useState(false)
+  const { isLoading, error, data, getData } = useVisitorData({ extendedResult }, { immediate: true })
+
+  const reloadData = () => {
+    getData({ ignoreCache: true })
+  }
+
+  const onChangeExtendedResult = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateExtendedResult(e.target.checked)
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>FingerprintJS Pro NextJS Demo</h1>
+      <div className={styles.testArea}>
+        <div className={styles.description}>
+          Lets load FingerprintJS Pro Agent using react integration and check next things:
+        </div>
+        <ol className={styles.actionPoints}>
+          <li>There is no errors on server</li>
+          <li>There is no errors on client</li>
+          <li>In the field below visitor data was loaded</li>
+          <li>Try controls to test additional params</li>
+        </ol>
+        <div className={styles.controls}>
+          <button onClick={reloadData} type='button'>
+            Reload data
+          </button>
+          <label>
+            <input type='checkbox' onChange={onChangeExtendedResult} checked={extendedResult} />
+            Extended result
+          </label>
+        </div>
+        <h4>
+          VisitorId: <span className={styles.visitorId}>{isLoading ? 'Loading...' : data?.visitorId}</span>
+        </h4>
+        <h4>Full visitor data:</h4>
+        <pre className={styles.data}>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
