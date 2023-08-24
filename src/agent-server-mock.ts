1d81bb08a9ad03948950554086cc1f78fe7963af
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-08-24 14:47:00 +0200

    chore: remove unused file

--- src/agent-server-mock.ts
+++
@@ -1,7 +0,0 @@
-import { Agent } from '@fingerprintjs/fingerprintjs-pro-spa'
-
-export const agentServerMock: Agent = {
-  get: () => {
-    throw new Error("Don't use FingerprintJS Pro on server side")
-  },
-}
